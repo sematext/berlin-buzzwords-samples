98d10d252bf629ae954784217125d83515f60c87
gr0 <r.kuc@solr.pl>
2014-04-23 16:26:53 +0200

    Scaling part of Solr commands

--- 2014/solr_commands.sh
+++ 2014/solr_commands.sh
@@ -2,23 +2,27 @@ echo
 echo =================
 echo "download"
 echo =================
-wget http://www.eu.apache.org/dist/lucene/solr/4.7.2/solr-4.7.2.tgz
+wget http://www.eu.apache.org/dist/lucene/solr/4.8.0/solr-4.8.0.tgz
 
 echo
 echo =================
 echo "extract and get in"
 echo =================
-tar zxf solr-4.7.2.tgz
-cd solr-4.7.2/example/
+tar zxf solr-4.8.0.tgz
+rm -r solr-4.8.0/example/solr/collection1
+mkdir node2
+mkdir node3
+cp -R solr-4.8.0/example/* node2/
+cp -R solr-4.8.0/example/* node3/
+cd solr-4.8.0/example/
 
 echo
 echo =================
 echo "starting..."
 echo =================
-rm -r ./solr/collection1
-java -jar -DzkRun -Dcollection.configName=bbuzz_less -Dbootstrap_confdir=./example-schemaless/solr/collection1/conf start.jar --daemon &
+java -jar -DzkRun -Dcollection.configName=bbuzz_less -Dhost=127.0.0.1 -Dbootstrap_confdir=./example-schemaless/solr/collection1/conf start.jar --daemon &
 # wait for Solr to start - ZooKeeper will be started at 9983 port 
-ERROR=1; while [ ! $ERROR -eq 0 ]; do sleep 1; curl 'localhost:8983/solr/admin/ping'; ERROR=$?; done
+ERROR=1; while [ ! $ERROR -eq 0 ]; do sleep 1; curl 'localhost:8983/solr/admin/ping' > /dev/null; ERROR=$?; done
 tail -20 logs/solr.log
 
 echo
@@ -54,7 +58,7 @@ echo
 echo =================
 echo "upload new config"
 echo =================
-cd ../solr-4.7.2/example/
+cd ../solr-4.8.0/example/
 chmod +x scripts/cloud-scripts/zkcli.sh
 ./scripts/cloud-scripts/zkcli.sh -cmd upconfig -zkhost localhost:9983 -confdir ../../config/solr/ -confname bbuzz
 
@@ -93,7 +97,44 @@ echo
 echo =================
 echo "Scale out"
 echo =================
-echo "TODO"
+curl 'http://localhost:8983/solr/admin/collections?action=DELETE&name=bbuzz'
+
+echo
+echo =================
+echo "Scale out - creating collection..."
+echo =================
+curl 'http://localhost:8983/solr/admin/collections?action=CREATE&name=scale-this&numShards=1&replicationFactor=1&collection.configName=bbuzz'
+
+# start node2 - on port 7983
+cd ../node2
+java -jar -DzkHost=localhost:9983 -Dhost=127.0.0.1 -Djetty.port=7983 start.jar --daemon &
+ERROR=1; while [ ! $ERROR -eq 0 ]; do sleep 1; curl 'localhost:7983/solr/admin/ping' > /dev/null; ERROR=$?; done
+
+echo
+echo =================
+echo "Scale out - adding replica..."
+echo =================
+curl 'http://localhost:8983/solr/admin/collections?action=ADDREPLICA&shard=shard1&collection=scale-this&node=127.0.0.1:7983_solr'
+
+echo
+echo =================
+echo "Scale out - starting third node..."
+echo =================
+cd ../node3
+java -jar -DzkHost=localhost:9983 -Dhost=127.0.0.1 -Djetty.port=6983 start.jar --daemon &
+ERROR=1; while [ ! $ERROR -eq 0 ]; do sleep 1; curl 'localhost:6983/solr/admin/ping' > /dev/null; ERROR=$?; done
+
+echo
+echo =================
+echo "Scale out - adding another replica..."
+echo =================
+curl 'http://localhost:8983/solr/admin/collections?action=ADDREPLICA&shard=shard1&collection=scale-this&node=127.0.0.1:6983_solr'
+
+echo
+echo =================
+echo "Scale out - split shard..."
+echo =================
+curl 'http://localhost:8983/solr/admin/collections?action=SPLITSHARD&collection=scale-this&shard=shard1'
 
 echo
 echo =================
